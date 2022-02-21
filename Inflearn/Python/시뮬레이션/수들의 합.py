3059b5d5f9f8a6dbe4a7d29ff5991f492b2fcbd1
JisinKeo <97288251+JisinKeo@users.noreply.github.com>
2022-02-21 13:50:38 +0900

    Create 수들의 합.py

---
+++ Inflearn/Python/시뮬레이션/수들의 합.py
@@ -0,0 +1,50 @@
+'''
+N개의 수로 된 수열 A[1], A[2], …, A[N] 이 있다. 이 수열의 i번째 수부터 j번째 수까지의 
+합 A[i]+A[i+1]+…+A[j-1]+A[j]가 M이 되는 경우의 수를 구하는 프로그램을 작성하시오.
+▣ 입력설명
+첫째 줄에 N(1≤N≤10,000), M(1≤M≤300,000,000)이 주어진다. 다음 줄에는 A[1], A[2], …, 
+A[N]이 공백으로 분리되어 주어진다. 각각의 A[x]는 30,000을 넘지 않는 자연수이다.
+▣ 출력설명
+첫째 줄에 경우의 수를 출력한다.
+▣ 입력예제 1 
+8 3
+1 2 1 3 1 1 1 2
+▣ 출력예제 1
+5
+'''
+import sys
+#sys.stdin = open("input.txt", "rt")
+
+n, m = map(int, input().split())
+
+number = list(map(int, input().split()))
+cnt = 0
+lt = 0
+rt = 1
+temp = number[0]
+while True:
+    if temp < m:
+        if rt < n:
+            temp += number[rt]
+            rt += 1
+        else:
+            break
+    elif temp == m:   
+        cnt += 1
+        lt += 1
+        if lt == n:
+            break
+        temp = number[lt]
+        rt = lt + 1
+  
+    else:
+        lt += 1
+        if lt == n:
+            break
+        temp = number[lt]
+        rt = lt + 1
+
+
+    print(lt, rt, temp)
+    
+print(cnt)
