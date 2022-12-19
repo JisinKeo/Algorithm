aac99ffc27079c8e7b5a895941256602c921c704
JisinKeo <97288251+JisinKeo@users.noreply.github.com>
2022-12-19 18:21:37 +0900

    Update 수열 추측하기.py

--- Inflearn/Python/DFS/수열 추측하기.py
+++ Inflearn/Python/DFS/수열 추측하기.py
@@ -1,27 +1,55 @@
+import sys
 
-def DFS(L, sum):
-    if L==n and sum==f:
-        for x in p:
-            print(x, end=' ')
-        sys.exit(0)
+def DFS(v, sum):
+
+    if v == n:
+        if sum == f:
+            print(result)
+            sys.exit(0)
+
     else:
         for i in range(1, n+1):
-            if ch[i]==0:
-                ch[i]=1
-                p[L]=i
-                DFS(L+1, sum+(p[L]*b[L]))
-                ch[i]=0
+            if checked[i] == 0:
+                checked[i] = 1
+                result[v] = i
+                DFS(v+1, sum + (bc[v] * result[v]))
+                checked[i] = 0
+                
+
+
+
+
+
+
 
 if __name__ == "__main__":
-    n, f=map(int, input().split())
-    p=[0]*n
-    b=[1]*n
-    ch=[0]*(n+1)
+ 
+    n, f = map(int, input().split())
+
+    result = [0] * n
+
+    checked = [0] * (n+1)
+
+    numerator = [1] * n
+
+    denumerator = [1] * n
+
+    bc = [1] * n
+
     for i in range(1, n):
-        b[i]=b[i-1]*(n-i)//i
-    DFS(0, 0)
+        numerator[i] = numerator[i-1] * (n-i)
+
+    for j in range(1, n):
+        denumerator[j] = denumerator[j-1] * j
 
 
+    for k in range(1, n):
+        bc[k] = numerator[k] // denumerator[k]
+
+
+    for i in range(1, n+1):
+
+        DFS(0, 0)
 
 
 
