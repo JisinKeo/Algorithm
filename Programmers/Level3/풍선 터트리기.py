a0f11a630e1d951bf50d79d886f6dcbd82c8878d
김민석 <tntjd9514@naver.com>
2024-06-28 02:02:38 +0900

    Create 풍선 터트리기.py

---
+++ Programmers/Level3/풍선 터트리기.py
@@ -0,0 +1,24 @@
+def solution(a):
+    answer = 0
+    
+    n = len(a)
+    
+    left_min = [0] * n
+    right_min = [0] * n
+    
+    left_min[0] = a[0]
+    
+    for i in range(1, n):
+        left_min[i] = min(left_min[i-1], a[i])
+    
+    right_min[-1] = a[-1]
+    for i in range(n-2, -1, -1):
+        right_min[i] = min(right_min[i+1], a[i])
+        
+    
+    for i in range(n):
+        if a[i] == left_min[i] or a[i] == right_min[i]:
+            answer += 1
+        
+
+    return answer
